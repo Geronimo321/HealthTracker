caa888c2ed5e4be5be86643c94ee332014fbf06b
Geronimo321 <76615426+Geronimo321@users.noreply.github.com>
2023-03-13 06:20:40 -0600

    FirstAngularCommit

---
+++ HealthTrackerBackend/src/test/java/com/example/HealthTrackerBackend/HealthTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthTrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
